2daa121e73157a1228ff738efd3fbc5d588aee7d
LegrandNico <LegrandNico@users.noreply.github.com>
2025-01-27 13:05:48 +0000

    Deploying to gh-pages from @ ComputationalPsychiatry/pyhgf@c65e16176ec24c1187ebb892d86cd766b5f49b46 🚀

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.0.post1.dev0+dbad150',
+    VERSION: '0.0.0.post1.dev0+c65e161',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
