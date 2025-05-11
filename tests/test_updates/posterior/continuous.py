96abe26af1fb842b351e148c2267da66db503985
LegrandNico <nicolas.legrand@cas.au.dk>
2025-05-11 22:38:22 +0200

    add tests

--- tests/test_updates/posterior/continuous.py
+++ tests/test_updates/posterior/continuous.py
@@ -45,8 +45,8 @@ def test_continuous_posterior_updates():
     new_attributes = continuous_node_posterior_update(
         attributes=attributes, node_idx=2, edges=edges
     )
-    assert jnp.isclose(new_attributes[1]["mean"], -0.0021212)
-    assert jnp.isclose(new_attributes[1]["precision"], 1.0022112)
+    assert jnp.isclose(new_attributes[2]["mean"], -0.0021212)
+    assert jnp.isclose(new_attributes[2]["precision"], 1.0022112)
 
     # eHGF updates ---------------------------------------------------------------------
     # ----------------------------------------------------------------------------------
@@ -73,5 +73,21 @@ def test_continuous_posterior_updates():
     new_attributes = continuous_node_posterior_update_ehgf(
         attributes=attributes, node_idx=2, edges=edges
     )
-    assert jnp.isclose(new_attributes[1]["mean"], -0.00212589)
-    assert jnp.isclose(new_attributes[1]["precision"], 1.0022112)
+    assert jnp.isclose(new_attributes[2]["mean"], -0.00212589)
+    assert jnp.isclose(new_attributes[2]["precision"], 1.0022112)
+
+    # unbounded updates ----------------------------------------------------------------
+    # ----------------------------------------------------------------------------------
+
+    # volatility update
+    attributes, edges, _ = network.get_network()
+    attributes[1]["temp"]["current_variance"] = 1.0
+    attributes[1]["expected_precision"] = 0.9820137619972229
+    attributes[1]["mean"] = 0.5225493907928467
+    attributes[1]["precision"] = 1.9820137023925781
+
+    new_attributes = continuous_node_posterior_update_unbounded(
+        attributes=attributes, node_idx=2, edges=edges
+    )
+    assert jnp.isclose(new_attributes[2]["mean"], -0.0019574)
+    assert jnp.isclose(new_attributes[2]["precision"], 1.0088315)
